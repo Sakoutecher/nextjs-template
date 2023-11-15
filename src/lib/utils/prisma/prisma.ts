d161174bf9772b0495f0978c324d15bc31d9f9cc
hcampos <hcampospro@gmail.com>
2023-11-15 12:46:26 +0100

    chore(package.json): add "@prisma/client" dependency to enable usage of Prisma ORM feat(prisma): add Prisma schema file with example schema from Prisma docs feat(prisma): add Prisma client initialization file for database operations using Prisma ORM

---
+++ src/lib/utils/prisma/prisma.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { PrismaClient } from "@prisma/client";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const prisma: PrismaClient = global.prisma ?? new PrismaClient({
+  log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"]
+});
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
